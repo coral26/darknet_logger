d5e80fbddaa5d1b8677bca6baed0a7b39f5019ba
Coral Moreno <coral26@gmail.com>
2019-04-11 18:09:54 -0400

    changed the subscribed topic to work from the vehicle side

--- nodes/darknet_logger_node.py
+++ nodes/darknet_logger_node.py
@@ -17,7 +17,7 @@ def darknet_logger():
 	timestamp = datetime.datetime.utcfromtimestamp(rospy.Time.now().to_time()).isoformat()
 	bag = rosbag.Bag('nodes/darknet_ros_'+('-'.join(timestamp.split(':')))+'.bag', 'w', rosbag.Compression.BZ2)
 
-	rospy.Subscriber('/udp/darknet_ros/bounding_boxes',BoundingBoxes,callback)
+	rospy.Subscriber('/darknet_ros/bounding_boxes',BoundingBoxes,callback)
 	rospy.spin()
 
 	bag.close()
