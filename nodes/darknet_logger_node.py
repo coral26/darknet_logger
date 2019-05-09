8bd0718288df69e7b25172f59ebce620757533b7
Coral Moreno <coral26@gmail.com>
2019-05-09 11:31:16 -0400

    some modifications

--- nodes/darknet_logger_node.py
+++ nodes/darknet_logger_node.py
@@ -3,12 +3,18 @@
 import rospy
 import rosbag
 import datetime
+from sensor_msgs.msg import Image
 from darknet_ros_msgs.msg import BoundingBoxes
 
-def callback(data):
+def callback_BB(data):
 	#print data
 	#print
 	bag.write('/darknet_ros/bounding_boxes',data)
+
+def callback_DI(data):
+	#print data
+	#print
+	bag.write('/darknet_ros/detection_image',data)
 
 def darknet_logger():
 	rospy.init_node('darknet_logger', anonymous=False)
@@ -16,9 +22,10 @@ def darknet_logger():
 
 	timestamp = datetime.datetime.utcfromtimestamp(rospy.Time.now().to_time()).isoformat()
 	#bag = rosbag.Bag('nodes/darknet_ros_'+('-'.join(timestamp.split(':')))+'.bag', 'w')
-    bag = rosbag.Bag('/media/exfat/jetson2/nodes/darknet_ros_'+('-'.join(timestamp.split(':')))+'.bag', 'w')
+        bag = rosbag.Bag('/media/exfat/jetson2/nodes/darknet_ros_'+('-'.join(timestamp.split(':')))+'.bag', 'w')
 
-	rospy.Subscriber('/darknet_ros/bounding_boxes',BoundingBoxes,callback)
+	rospy.Subscriber('/darknet_ros/bounding_boxes',BoundingBoxes,callback_BB)
+        rospy.Subscriber('/darknet_ros/detection_image',Image,callback_DI)
 	rospy.spin()
 
 	bag.close()
